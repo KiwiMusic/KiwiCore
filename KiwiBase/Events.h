1c7f0497b9d895de2566e3acc0a95cb98c635507
pierreguillot <guillotpierre6@gmail.com>
2014-11-10 14:26:24 +0100

    Events

---
+++ KiwiBase/Events.h
@@ -0,0 +1,14 @@
+//
+//  Events.h
+//  KiwiCore
+//
+//  Created by Guillot Pierre on 10/11/2014.
+//  Copyright (c) 2014 PierreGuillot. All rights reserved.
+//
+
+#ifndef __KiwiCore__Events__
+#define __KiwiCore__Events__
+
+#include <iostream>
+
+#endif /* defined(__KiwiCore__Events__) */
