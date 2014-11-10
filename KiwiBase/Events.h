d64b177715c2827b7fad5d39ad684f41818b636c
pierreguillot <guillotpierre6@gmail.com>
2014-11-10 14:29:46 +0100

    Events !

--- KiwiBase/Events.h
+++ KiwiBase/Events.h
@@ -11,4 +11,48 @@
 
 #include <iostream>
 
+namespace Kiwi
+{
+    class Events
+    {
+    public:
+        enum Type
+        {
+            Unknown     = 0,
+            Enter       = 1,
+            Leave       = 2,
+            Move        = 3,
+            Drag        = 4,
+            Down        = 5,
+            Up          = 6,
+            DoubleClick = 7,
+            Wheel       = 8
+        };
+        
+        enum Modifier
+        {
+            Nothing     = 0,
+            Shift       = 1,
+            Ctrl        = 2,
+            Alt         = 4,
+            Left        = 16,
+            Right       = 32,
+            Middle      = 64,
+            
+#ifdef __APPLE__
+            Cmd          = 8,
+            Popup        = Right | Ctrl,
+#else
+            Cmd          = Ctrl,
+            Popup        = Right
+#endif
+        };
+        
+        const Type      type;
+        const int       x;
+        const int       y;
+        const long      modifiers;
+        const double    wheel;
+    };
+}
 #endif /* defined(__KiwiCore__Events__) */
