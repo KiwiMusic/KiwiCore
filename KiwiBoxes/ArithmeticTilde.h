d4a131e80a81fc9dc732d1f1773275eabedf78d4
pierreguillot <guillotpierre6@gmail.com>
2015-01-19 15:46:57 +0100

    Dsp + dsp boxe

--- KiwiBoxes/ArithmeticTilde.h
+++ KiwiBoxes/ArithmeticTilde.h
@@ -25,10 +25,49 @@
 #define __DEF_KIWI_ARITHMETIC_TILDE__
 
 #include "../KiwiBase/Core.h"
+#include "../KiwiDsp/Dsp.h"
 
 namespace Kiwi
 {
-    ;
+    class SigTilde : public Box, public Dsp::Sig
+    {
+    public:
+        
+        SigTilde(sPage page) : Box(page, "sig~")
+        {
+            addInlet(Iolet::Message, Iolet::Hot, "Vamllll 1");
+        }
+
+        SigTilde(sPage page, const double value) : Box(page, "sig~")
+        {
+            ;
+        }
+
+        
+        ~SigTilde()
+        {
+            ;
+        }
+        
+        bool receive(ulong index, ElemVector const& elements) override
+        {
+            if(!elements.empty() && elements[0].isNumber())
+            {
+                Dsp::Sig::setValue(elements[0]);
+                return  true;
+            }
+            else
+            {
+                return  false;
+            }
+        }
+    
+        sBox allocate(sPage page, sDico dico) const override
+        {
+            
+            return make_shared<SigTilde>(page);
+        }
+    };
 }
 
 #endif
