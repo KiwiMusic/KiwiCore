6575d1e6a75ea17fd13d0a5d2d9ba018b01983cb
pierreguillot <guillotpierre6@gmail.com>
2015-01-21 08:03:54 +0100

    Link for view

--- KiwiBase/Link.h
+++ KiwiBase/Link.h
@@ -308,19 +308,6 @@ namespace Kiwi
                 ;
             }
             
-            //! The controller maker.
-            /** The function creates an controller with arguments.
-             */
-            template<class CtrlClass, class ...Args> static shared_ptr<CtrlClass> create(Args&& ...arguments)
-            {
-                shared_ptr<CtrlClass> ctrl =  make_shared<CtrlClass>(forward<Args>(arguments)...);
-                if(ctrl && ctrl->m_link)
-                {
-                    ctrl->m_link->setController(ctrl);
-                }
-                return ctrl;
-            }
-            
             //! Retrieve the link.
             /** The funtion retrieves the link.
              @return The link.
@@ -330,174 +317,14 @@ namespace Kiwi
                 return m_link;
             }
 			
-			//! Notify that the link is selected.
-			/** The function notifies that the link is selected and needs to be redrawn.
-			 @param status true if the link is selected, otherwise false.
-			 */
-			void setSelectedStatus(bool status);
-			
-			//! Retrieve if the link is selected.
-			/** The function retrieves if the link is selected.
-			 @param True if the link is selected, otherwise false.
-			 */
-			inline bool isSelected() const noexcept
-			{
-				return m_selected;
-			}
-            
-            //! Retrieve if the link is touch by a point.
-			/** The function retrieves if the link is touch by a point and fill the knock with the knock informations.
-             @param point The point.
-             @param knock The knock.
-			 @return true if the link is touch by the point, otherwise false.
-			 */
-			virtual bool contains(Gui::Point const& point, Knock& knock) const noexcept;
-            
-            //! Retrieve if the link is overlaps the rectangle.
-			/** The function retrieves if the link is overlaps the rectangle.
-             @param rect The Rectangle.
-			 @return true if the link overlaps the rectangle, otherwise false.
-			 */
-			virtual bool overlaps(Gui::Rectangle const& rect) const noexcept;
-			
             //! The bounds notification function that should be override.
             /** The function is called by the link when its bounds changed.
              */
             virtual void boundsChanged() = 0;
-            
-            //! The redraw function that should be override.
-            /** The function is called by the link when it should be repainted.
-             */
-            virtual void redraw() = 0;
-            
-            //! The default paint method.
-            /** The default function paint a default link.
-             @param link        The link to draw.
-             @param d           The doodle to draw in.
-             @param selected    If the link is selected
-             */
-            static void paint(sLink link, Gui::Doodle& d, bool selected = false);
-        };
-        
-        // ================================================================================ //
-        //                                  LINK CREATOR                                    //
-        // ================================================================================ //
-        
-        class Creator
-        {
-        private:
-            wBox			m_box_from;
-            wBox			m_box_to;
-            ulong   m_index_outlet;
-            ulong   m_index_intlet;
-            Gui::Path       m_path;
-            
-        public:
-            
-            //! The constructor.
-            /** You should never use this method.
-             */
-            Creator(const sBox from, const unsigned outlet, const sBox to, const unsigned inlet) noexcept;
-            
-            //! The destructor.
-            /** You should never use this method.
-             */
-            ~Creator();
-            
-            //! The creator creation method with sockets.
-            /** The function allocates a creator.
-             @param from    The box that send.
-             @param outlet  The index of the outlet.
-             @param to      The box that receive.
-             @param inlet   The index of the inlet.
-             @return The creator.
-             */
-            static shared_ptr<Creator> create(const sBox from, const unsigned outlet, const sBox to, const unsigned inlet);
-            
-            //! Retrieve if the creator owns the boxes.
-            /** The function retrieves if the creator owns the two boxes.
-             @return true if the creator owns the two boxes, otherwise false.
-             */
-            inline bool hasBoxes() const noexcept
-            {
-                return !Creator::m_box_from.expired() && Creator::m_box_to.expired();
-            }
-            
-            //! Retrieve if the creator owns the output box.
-            /** The function retrieves if the creator owns the output box.
-             @return true if the creator owns the output box, otherwise false.
-             */
-            inline bool hasBoxFrom() const noexcept
-            {
-                return !Creator::m_box_from.expired();
-            }
-            
-            //! Retrieve if the creator owns the input box.
-            /** The function retrieves if the creator owns the input box.
-             @return true if the creator owns the input box, otherwise false.
-             */
-            inline bool hasBoxTo() const noexcept
-            {
-                return !Creator::m_box_to.expired();
-            }
-            
-            //! Retrieve the index of the outlet of the creator.
-            /** The function retrieves the index of the outlet of the creator.
-             @return The index of the outlet of the creator.
-             */
-            inline ulong getOutletIndex() const noexcept
-            {
-                return Creator::m_index_outlet;
-            }
-            
-            //! Retrieve the index of the inlet of the creator.
-            /** The function retrieves the index of the inlet of the creator.
-             @return The index of the inlet of the creator.
-             */
-            inline ulong getInletIndex() const noexcept
-            {
-                return Creator::m_index_intlet;
-            }
-            
-            //! Retrieve the output box.
-            /** The function retrieves the output box of the creator.
-             @return The output box.
-             */
-            inline sBox getBoxFrom() const noexcept
-            {
-                return m_box_from.lock();
-            }
-            
-            //! Retrieve the input box.
-            /** The function retrieves the input box of the creator.
-             @return The input box.
-             */
-            inline sBox getBoxTo() const noexcept
-            {
-                return m_box_to.lock();
-            }
-            
-            /** Call this in your mouseDrag event, to update the link's position.
-             This must be repeatedly calling when the mouse is dragged, after you've
-             first initialised the link with beginLink().
-             */
-            void drag(Gui::Point const& point)
-            {
-                if(m_path.size() == 1)
-                {
-                    m_path.lineTo(point);
-                }
-                else
-                {
-                    m_path.setPoint(m_path.size() - 1, point);
-                }
-            }
         };
         
         static const sTag Tag_from;
         static const sTag Tag_to;
-        
-        static bool compareBoxToPositions(sLink link1, sLink link2);
     };
     
     static bool operator==(scLink link, scBox box) noexcept
